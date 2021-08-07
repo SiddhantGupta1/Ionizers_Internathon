716ea2a2e69af10965be61d12712b2553cdf0030
SiddhantGupta1 <sg4714@srmist.edu.in>
2021-08-07 20:56:45 +0530

    Home Page Completed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './login.js'
 import Explore from './explore.js'
-
+import HomeIcon from './Icons/HomeIcon.svg'
+import AddIcon from './Icons/AddIcon.svg'
+import ExploreIcon from './Icons/ExploreIcon.svg'
+import UserIcon from './Icons/UserIcon.svg'
+import Card1 from './Icons/Card1.svg'
+import Card2 from './Icons/Card2.svg'
+import moreInfo from './Icons/moreInfo.svg'
+import Line from './Icons/Line.svg'
 
 const App = () => {
   if(window.location.pathname !== "/explore")
   {
     document.getElementById('mapid').style.display = "none";
   }
-
   return (
       <Router>
         <Switch>
-          <Route path="/" exact component={Login} />
+          <Route path="/" exact>
+            <Login />
+          </Route>
           <Route path="/home" component={Home} />
           <Route path="/explore" component={Explore} />
         </Switch>
@@ -23,18 +31,102 @@ const App = () => {
 }
 
 const Home = () => {
-  
+  const data = [
+    {
+      id : 1,
+      pic : Card1,
+      location: "London: The heritage of England",
+      description: "Standing on the Thames River, London is the capital of England as well as the most populous city in Great Britain. The city is a confluence of the old and the new and is one of the most important tourist destinations in the entire world. It is a city with numerous attractions of all sorts including great architecture and heritage."
+    },
+    {
+      id : 2,
+      pic: Card2,
+      location: "Bora Bora Tourism",
+      description: "A part of French Polynesia, this magical island has some of the most beautiful sights to behold"
+    },
+    {
+      id: 3,
+      pic : Card1,
+      location: "London: The heritage of England",
+      description: "Standing on the Thames River, London is the capital of England as well as the most populous city in Great Britain. The city is a confluence of the old and the new and is one of the most important tourist destinations in the entire world. It is a city with numerous attractions of all sorts including great architecture and heritage."
+    },
+    {
+      id : 4,
+      pic: Card2,
+      location: "Bora Bora Tourism",
+      description: "A part of French Polynesia, this magical island has some of the most beautiful sights to behold"
+    },
+    {
+      id : 5,
+      pic : Card1,
+      location: "London: The heritage of England",
+      description: "Standing on the Thames River, London is the capital of England as well as the most populous city in Great Britain. The city is a confluence of the old and the new and is one of the most important tourist destinations in the entire world. It is a city with numerous attractions of all sorts including great architecture and heritage."
+    },
+    {
+      id : 6,
+      pic: Card2,
+      location: "Bora Bora Tourism",
+      description: "A part of French Polynesia, this magical island has some of the most beautiful sights to behold"
+    },
+    {
+      id : 7,
+      pic : Card1,
+      location: "London: The heritage of England",
+      description: "Standing on the Thames River, London is the capital of England as well as the most populous city in Great Britain. The city is a confluence of the old and the new and is one of the most important tourist destinations in the entire world. It is a city with numerous attractions of all sorts including great architecture and heritage."
+    },
+  ]
+  var flag;
+
+const imageClick = (e) => {
+  const blur = document.getElementById(`image${e}`)
+  const location = document.getElementById(`location${e}`)
+  const description = document.getElementById(`description${e}`)
+  const btn = document.getElementById(`img-btn${e}`)
+
+  if(flag === 1){
+    blur.style.filter = "blur(0) brightness(100%)"
+    location.style.opacity = 0
+    description.style.opacity = 0
+    btn.style.opacity = 0
+    flag=0
+    console.log(flag)
+  }
+  else{
+    blur.style.filter = "blur(2px) brightness(40%)"
+    location.style.opacity = 1
+    description.style.opacity = 1
+    btn.style.opacity = 1
+    flag=1
+    console.log(flag)
+  }
+}
+
   return(
+    <div style={{display:"flex", justifyContent:"center", backgroundColor:"coral"}}>
       <div className="App">
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <a href="/">Login</a>
-        <a href="/explore">Explore</a>
+        <div className="header">
+          <img id="" src={HomeIcon} alt="" />
+          <img id="" src={Line} alt=""/>
+          <img id="" src={AddIcon} alt="" onClick={()=> { window.location.href = "/upload" }} />
+          <img id="" src={ExploreIcon} alt="" onClick={()=> { window.location.href = "/explore" }} />
+          <img id="" src={UserIcon} alt="" />
+        </div>
+        <p>Hiii, <br /> Internathon</p>
+        <article>Top Picks</article>
+
+        <div style={{display:"flex", flexWrap:"wrap"}}>
+          {data.map(e => (
+            <div className="cards" key={e.id} onClick={()=>{imageClick(e.id) } } >
+              <img id={"image"+e.id} src={e.pic} alt=""/>
+              <p id={"location"+e.id} >{e.location}</p>
+              <article id={"description"+e.id}>{e.description}</article>
+              <button id={"img-btn"+e.id}><img src={moreInfo} alt="" /></button>
+            </div>
+          ))}
+        </div>
+        
       </div>
+    </div>
   )
 }
 
--- src/login.js
+++ src/login.js
@@ -1,79 +1,172 @@
 import React from 'react'
 import './login.css'
-import Google from './Icons/Google.svg'
-import Facebook from './Icons/Facebook.svg'
-import Apple from './Icons/Apple.svg'
+import Google from './Icons/Google.png'
+import Facebook from './Icons/Facebook.png'
+import Apple from './Icons/Apple.png'
 import greaterThan from './Icons/greaterThan.svg'
 import Plane1 from './Icons/Plane1.svg'
 import Plane2 from './Icons/Plane2.svg'
 import Plane3 from './Icons/Plane3.svg'
 
+
+var flag = 1
 const login = () => {
-    
+
+    const shiftAnim = () => {
+        const Login = document.getElementById('Login')
+        const Login_Signup = document.getElementById('Login-Signup')
+        const Signup = document.getElementById('Signup')
+
+        if(flag === 1)
+        {
+            Login_Signup.style.transform = "translateX(0%)"
+            Login.style.transform = "translateX(0%)"
+            Signup.style.transform = "translateX(0%)"
+
+        }
+        else if(flag === 2)
+        {
+            Login_Signup.style.transform = "translateX(-100%)"
+            Login.style.transform = "translateX(-100%)"
+            Signup.style.transform = "translateX(-100%)"
+
+        }
+        else if(flag === 3)
+        {
+            Login_Signup.style.transform = "translateX(-200%)"
+            Login.style.transform = "translateX(-200%)"
+            Signup.style.transform = "translateX(-200%)"
+        }
+    }
+
+    const loginAnim = () => {
+        shiftAnim()
+        document.getElementById('Login-heading').style.transform = "translateX(-334px)"
+        const gotoLogin = document.getElementById('gotoLogin')
+
+        gotoLogin.style.transform = "translate(-327px)"
+        gotoLogin.style.width = "255px"
+        flag = 2
+    }
+
+    const gofromLoginAnim = () => {
+        shiftAnim()
+        document.getElementById('Login-heading').style.transform = "translateX(0%)"
+        const gotoLogin = document.getElementById('gotoLogin')
+
+        gotoLogin.style.transform = "translate(0%)"
+        gotoLogin.style.width = "175px"
+        flag=1
+    }
+
+    const signupAnim = () => {
+        flag=3
+        shiftAnim()
+        document.getElementById('Signup-heading').style.transform = "translateX(317px)"
+        const gotoSignup = document.getElementById('gotoSignup')
+
+        gotoSignup.style.transform = "translate(247px, 95px)"
+        gotoSignup.style.width = "255px"
+        flag=2
+    }
+
+    const gofromSignupAnim = () => {
+        shiftAnim()
+        document.getElementById('Signup-heading').style.transform = "translateX(0%)"
+        const gotoSignup = document.getElementById('gotoSignup')
+
+        gotoSignup.style.transform = "translate(0%)"
+        gotoSignup.style.width = "175px"
+        flag=1
+    }
+
+    const loginSubmit = () => {
+        console.log("Login")
+        window.location.href = "/home"
+    }
+    const signupSubmit = () => {
+        console.log("Signup")
+    }
 
     return (
-        <div className="login">
-            <div className="Login">
-                <header>Travello</header>
-                <p>Login</p>
-                <input type="text" placeholder="Email ID" />
-                <input type="password" placeholder="Password" />
-                <button id="login-btn">Login</button>
-                <section>
-                    <img src={Google} alt="Google"/>
-                    <img src={Facebook} alt="Facebook"/>
-                    <img src={Apple} alt="Apple ID"/>
-                </section>
-                <button id="login-page-btn"><img src={greaterThan} alt="Greater than symbol" /></button>
-                <img id="Plane1" src={Plane1} alt=""/>
-            </div>
+        <div style={{display:"flex", justifyContent:"center", backgroundColor:"coral"}}>
+            <div className="login" id="login">
+                <div className="Login" id="Login">
+                    <header>Travello</header>
+                    <p> </p>
+                    <input type="text" placeholder="Email ID" style={{marginTop:"45px"}} />
+                    <input type="password" placeholder="Password" />
 
-            <div className="Login-Signup">
-                <header>Travello</header>
-                <div>
                     <section>
-                        <h2>Login</h2>
-                        <p>
-                           <span>Existing User?</span><br/>
-                           Welcome Back!<br/>
-                           Login with ur credentials
-                        </p>
-                        <button>Login</button>
+                        <img src={Google} alt="Google"/>
+                        <img src={Facebook} alt="Facebook"/>
+                        <img src={Apple} alt="Apple ID"/>
                     </section>
-
-                    <article>
-                        <h2>Sign Up</h2>
-                        <p>
-                        <span>New User?</span><br/>
-                        We assure,<br/>
-                        U’ll have ur best tym here, Go ahead. 
-                        </p>
-                        <button>Sign Up</button>
-                    </article>
+                    <button onClick={gofromLoginAnim} id="login-page-btn"><img src={greaterThan} alt="Greater than symbol" /></button>
+                    <img id="Plane1" src={Plane1} alt=""/>
                 </div>
-                <img id="Plane2" src={Plane2} alt=""/>
-            </div>
 
-            <div className="Signup">
-                <header>Travello</header>
-                <p>Sign Up</p>
-                <div>
-                    <input id="firstname" type="text" placeholder="First Name" />
-                    <input id="lastname" type="text" placeholder="Last Name" />
-                    <input id="emailid" type="text" placeholder="Email id" />
-                    <input id="pass" type="password" placeholder="Set Password" />
-                    <input id="password" type="password" placeholder="Confirm Password" />
+                <div className="Login-Signup" id="Login-Signup">
+                    <header>Travello</header>
+                    <div>
+                        <section>
+                            <h2 id="Login-heading">Login</h2>
+                            <p>
+                                <span>Existing User?</span><br/>
+                                Welcome Back!<br/>
+                                Login with ur credentials
+                            </p>
+                            <button id="gotoLogin" onClick={ () =>{ 
+                                if(flag === 1){
+                                    loginAnim()
+                                }
+                                else{
+                                    loginSubmit()
+                                }
+                            } }>Login</button>
+                        </section>
+
+                        <article>
+                            <h2 id="Signup-heading">Sign Up</h2>
+                            <p>
+                                <span>New User?</span><br/>
+                                We assure,<br/>
+                                U’ll have ur best tym here, Go ahead. 
+                            </p>
+                            <button id="gotoSignup" onClick={ () =>{ 
+                                if(flag === 1){
+                                    signupAnim()
+                                }
+                                else{
+                                    signupSubmit()
+                                }
+                            } }>Sign Up</button>
+                        </article>
+                    </div>
+                    <img id="Plane2" src={Plane2} alt=""/>
                 </div>
-                <button id="signup-btn">Sign Up</button>
-                <section>
-                    <img src={Google} alt="Google"/>
-                    <img src={Facebook} alt="Facebook"/>
-                    <img src={Apple} alt="Apple ID"/>
-                </section>
-                <button id="signup-page-btn"><img src={greaterThan} alt="Greater than symbol" style={{transform:"scaleX(-1)"}} /></button>
-                <img id="Plane3" src={Plane3} alt=""/>
-            </div>
 
+                <div className="Signup" id="Signup">
+                    <header>Travello</header>
+                    <p> </p>
+                    <div style={{marginTop:"45px"}}>
+                        <input id="firstname" type="text" placeholder="First Name" />
+                        <input id="lastname" type="text" placeholder="Last Name" />
+                        <input id="emailid" type="text" placeholder="Email id" />
+                        <input id="pass" type="password" placeholder="Set Password" />
+                        <input id="password" type="password" placeholder="Confirm Password" />
+                    </div>
+                    
+                    <section>
+                        <img src={Google} alt="Google"/>
+                        <img src={Facebook} alt="Facebook"/>
+                        <img src={Apple} alt="Apple ID"/>
+                    </section>
+                    <button id="signup-page-btn" onClick={gofromSignupAnim} ><img src={greaterThan} alt="Greater than symbol" style={{transform:"scaleX(-1)"}} /></button>
+                    <img id="Plane3" src={Plane3} alt=""/>
+                </div>
+
+            </div>
         </div>
     )
 }
